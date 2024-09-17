1dffabf441de7935bfa74c8449595acfd1b8346f
Lewis <lewiskaranu0@gmail.com>
2024-09-17 23:01:35 +0300

    lib

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
 class Shoe:
-    pass
+    def __init__(self, brand="Adidas", size=9):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"  # Initialize condition as "Old"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    # Add the cobble method
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
